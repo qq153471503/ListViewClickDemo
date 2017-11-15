da4c7a30c9e0bbd2f06b889a193ab60b18b324ad
qq153471503 <153471503@qq.com>
2017-11-15 20:33:22 +0800

    Initial commit

---
+++ app/src/main/java/com/example/listviewclickdemo/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.listviewclickdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private ListView lvShow;
+    private String[] dataLv = {"1","2","3","4","5"};
+    private ArrayAdapter<String> adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, dataLv);
+        lvShow = (ListView)findViewById(R.id.id_lv_show);
+        lvShow.setAdapter(adapter);
+        lvShow.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "position : " + position,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
